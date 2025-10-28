bf7090c46ad28f70748c7e21aad1521bbad63161
Shannon Clark <src@ualberta.ca>
2025-10-27 23:52:07 -0600

    update config to export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
